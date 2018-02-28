898fa38241c55f56323f2911952d7ac84c3acc2f
Mike Bostock <mbostock@gmail.com>
2018-02-27 18:10:08 -0800

    Move build to dist.

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ export default {
   external: dependencies,
   output: {
     extend: true,
-    file: `build/${definition.name}.js`,
+    file: `dist/${definition.name}.js`,
     format: "umd",
     globals: dependencies.reduce((p, v) => (p[v] = "d3", p), {}),
     name: "d3"
