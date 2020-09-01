13dc6abd3af25df6d66f247a6422198981331311
Philippe Rivière <fil@rezo.net>
2020-09-01 10:32:03 +0200

    round tick values (upgrade to https://github.com/d3/d3-array/commit/04cf783bd715cae89303173d50a28fda38c24a9b)

--- test/contours-test.js
+++ test/contours-test.js
@@ -206,3 +206,19 @@ it("contours.size(…) validates the specified size", () => {
   assert.deepStrictEqual(contours().size([1.5, 2.5]).size(), [1, 2]);
   assert.throws(() => void contours().size([0, -1]), /invalid size/);
 });
+
+it("contours(values) returns the expected thresholds", () => {
+  const c = contours().size([10, 10]).thresholds(20);
+  assert.deepStrictEqual(c([
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 0, 1, 1, 1, 0, 0,
+    0, 1, 0, 1, 0, 1, 0, 1, 0, 0,
+    0, 1, 1, 1, 0, 1, 1, 1, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  ]).map(d => d.value), [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]);
+});
