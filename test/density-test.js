5e9ee8a5e9d1d16d91cdee0790db007a78d593b3
Philippe Rivière <fil@rezo.net>
2022-06-23 13:49:55 +0200

    respect thresholds passed to contourDensity

--- test/density-test.js
+++ test/density-test.js
@@ -16,7 +16,7 @@ it("contourDensity(data) returns the expected result for empty data", () => {
 });
 
 it("contourDensity(data) returns contours centered on a point", () => {
-  const c = contourDensity().thresholds([0.0001, 0.001]);
+  const c = contourDensity().thresholds([0.00001, 0.0001]);
   for (const p of [[100, 100], [100.5, 102]]) {
     const contour = c([p]);
     assert.strictEqual(contour.length, 2);
@@ -26,4 +26,24 @@ it("contourDensity(data) returns contours centered on a point", () => {
       assertInDelta(a[1], p[1], 0.1);
     }
   }
+});
+
+it("contourDensity.thresholds(values[])(data) returns contours for the given values", () => {
+  const points = [[1, 0], [0, 1], [1, 1]];
+  const c = contourDensity();
+  const c1 = c(points);
+  const values1 = c1.map(d => d.value);
+  const c2 = c.thresholds(values1)(points);
+  const values2 = c2.map(d => d.value);
+  assert.deepStrictEqual(values1, values2);
+});
+
+it("contourDensity.thresholds(values[])(data) returns contours for the given values at a different cellSize", () => {
+  const points = [[1, 0], [0, 1], [1, 1]];
+  const c = contourDensity().cellSize(16);
+  const c1 = c(points);
+  const values1 = c1.map(d => d.value);
+  const c2 = c.thresholds(values1)(points);
+  const values2 = c2.map(d => d.value);
+  assert.deepStrictEqual(values1, values2);
 });
